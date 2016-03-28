55ce3b0619031b6d2ffac0531c5a3cb5ff94061a
FireElement <pire_cao@163.com>
2016-03-28 13:52:09 +0800

    Modify map helper

---
+++ template/src/main/java/org/nightstudio/common/util/map/ListMapCvt.java
@@ -0,0 +1,8 @@
+package org.nightstudio.common.util.map;
+
+/**
+ * Created by xuezhucao on 16/3/28.
+ */
+public abstract class ListMapCvt<K, V> {
+    public abstract K getKey(V o);
+}
--- template/src/main/java/org/nightstudio/common/util/map/MapHelper.java
+++ template/src/main/java/org/nightstudio/common/util/map/MapHelper.java
@@ -2,25 +2,22 @@ package org.nightstudio.common.util.map;
 
 import org.apache.commons.collections.CollectionUtils;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Created by xuezhucao on 16/3/22.
  */
-public abstract class MapHelper<K, V> {
-    public abstract K getKey(V o);
-
-    public Map<K, V> getMap(Map<K, V> map, List<V> list) {
-        if (map == null) {
+public abstract class MapHelper {
+    public static <K, V> Map<K, V> getMap(List<V> list, ListMapCvt<K, V> cvt) {
+        if (CollectionUtils.isEmpty(list)) {
             return null;
         }
-        if (CollectionUtils.isEmpty(list)) {
-            return map;
-        }
+        Map<K, V> result = new HashMap<>(list.size());
         for (V o : list) {
-            map.put(getKey(o), o);
+            result.put(cvt.getKey(o), o);
         }
-        return map;
+        return result;
     }
 }
